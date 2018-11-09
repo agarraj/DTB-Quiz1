74851bdc34017b1a53d5fd3dd05f221eb651fd5d
tejus agarwal <tejusagarwal13@gmail.com>
2018-11-09 11:15:07 +0530

    Updated

--- TicTacToe.sol
+++ TicTacToe.sol
@@ -1,38 +1,43 @@
-// this is main file
-
 pragma solidity ^0.4.24;
 
-contract TTT
-{
-    uint8[] _board = new uint8[](9);
-    address _owner;
-    
-    address p1;
-    address p2;
-
-    bool _openSlots = true;
-    bool _gameInProgress = false;
-
-    uint8 _playerTurn;
-    uint _winner;
-
-    uint[][] _winStates = [[0,1,2],[3,4,5],[6,7,8], 
-                          [0,3,6],[1,4,7],[2,5,8], 
-                          [0,4,8],[2,4,6]];
-                          
-    constructor()
-    public {
-        _owner = msg.sender;
-    }
-
-     function registerPlayer1() private
+contract TicTacToe {
+    uint[] board = new uint[](9);
+    address player1;
+    address player2;
+    uint start = 0;
+   
+   
+    //This makes sure that quizMaster cannot become a particiant
+    modifier notPlayer1(){
+        require(msg.sender!=player1,"Player1 cannot become a Player2");
+    _;}
+   
+    constructor ()
+    public
     {
-            p1 = msg.sender;
+        player1 = msg.sender;
     }
-    
-    function registerPlayer2(address player2) private
+   
+    function joinGame()
+    public
+    notPlayer1()
     {
-            p2 = msg.sender;
+        player2 = msg.sender;
     }
+   
+    uint[][]  winning_Positions = [[0,1,2],[3,4,5],[6,7,8], [0,3,6],[1,4,7],[2,5,8], [0,4,8],[2,4,6]  ];
+   
+    function checkWinner()
+    public
+    constant
+    returns (uint)
+    {
+        for(uint i =0; i < 8;i++){
+            uint[] memory b = winning_Positions[i];
+            if(board[b[0]] != 0 && board[b[0]] == board[b[1]] && board[b[0]] == board[b[2]])
+                return board[b[0]];
+        }
 
-}
+   
+        return 0;
+    }
